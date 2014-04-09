68f001ac8e88164d633a0a85ec0f9ad599b07ebb
yandong <hanyandong@iie.ac.cn>
2014-04-09 16:06:59 +0800

    cdecl

---
+++ cdecl/build/CMakeFiles/cdecl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cdecl.dir/main.cpp.o"
+  "cdecl.pdb"
+  "cdecl"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cdecl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
